93716ac38c2e5c1685cca2896eeabdeb1ac9eaa3
Aidan Rikic <aidanrikic1020@gmail.com>
2024-05-28 09:58:44 -0700

    evrything good

--- grade.sh
+++ grade.sh
@@ -14,3 +14,37 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# check if file is right 
+if [ -f "student-submission/ListExamples.java" ]; then
+    echo 'This is the correct file' 
+else
+    echo 'This is not the right type of file! Please submit again'
+    exit 1
+fi
+
+# copy the files over to grading-area
+cp -r student-submission/*.java grading-area/
+cp -r TestListExamples.java grading-area/
+cp -r lib grading-area/
+
+# switch over and compile 
+cd grading-area
+javaC -cp $CPATH *.java 
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+# check for failures 
+if [ $? -eq 0 ]; then 
+    echo 'All tests passed'
+else 
+    echo 'Some tests failed!'
+    echo 'Check above to find out why'
+fi
+
+
+
+
+ 
+
+ 
+
